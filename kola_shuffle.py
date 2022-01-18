9869b4cdca7dc1d502d92e723087e93aa3906ce3
Bert Palm <bert.palm@ufz.de>
2022-01-18 02:09:29 +0100

    version 0.1

--- kola_shuffle.py
+++ kola_shuffle.py
@@ -8,27 +8,31 @@ import logging
 __author__ = "rosagold"
 __email__ = "meinschallundrauch@gmail.com"
 __copyright__ = "Me and only me and everyone else too"
+__version__ = "0.1.0"
 
-usage = """
+usage = f"""
 Usage: ./kola_shuffle.py FILE [NR] [OFFSET]
+Shuffle rows by column in an excel spreadsheet.
     
     FILE    input file (.xlsx) to shuffle
     NR      desired number of results (default 1)
     SKIP    header rows to skip (default 2)
-"""
 
-debug = False
-dflag = "--debug"
+version: {__version__}
+"""
 
 if __name__ == "__main__":
     args = sys.argv[1:]
     path, n, offset = None, 1, 2
 
-    if dflag in args:
-        logging.basicConfig(level="DEBUG", format="[%(asctime)s] %(levelname)s: %(message)s")
+    if "--debug" in args:
+        logging.basicConfig(
+            level="DEBUG", format="[%(asctime)s] %(levelname)s: %(message)s"
+        )
         logging.debug(f"args: {args}")
-        args.remove(dflag)
-        debug = True
+        args.remove("--debug")
+
+    logging.debug(f"version: {__version__}")
 
     if len(args) == 1:
         path = args[0]
@@ -44,6 +48,7 @@ if __name__ == "__main__":
     offset = int(offset)
     wb = openpyxl.load_workbook(filename=path, read_only=True)
 
+    # read file and use first sheet
     sheet = wb.sheetnames[0]
     logging.debug(f"sheet: {sheet}")
     sheet = wb[sheet]
@@ -62,8 +67,6 @@ if __name__ == "__main__":
     for _ in range(n):
         s = ""
         for c in range(1, sheet.max_column + 1):
-            v, r = None, None
-            # try random
             sample = random.sample(baselist, sz)
             for i, r in enumerate(sample):
                 v = sheet.cell(r, c).value
